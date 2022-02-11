dd68c2c02811744b3df7ad02199fd10036cd437e
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-12 00:12:32 +0530

    feat: added resuable form components

---
+++ src/components/FromComponents.js
@@ -0,0 +1,42 @@
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
+
+export const InputBox = ({ id, value, callback, type = "text" }) => {
+  return (
+    <>
+      <label htmlFor={id} className="capitalize text-lg">
+        {id}
+      </label>
+      <input
+        type={type}
+        id={id}
+        value={value}
+        placeholder={`Your ${id}`}
+        onChange={callback}
+        className="w-full rounded-md py-2 px-2 text-black my-2"
+      />
+    </>
+  );
+};
+
+export const PasswordInputField = ({ callback, value, id }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <>
+      <InputBox
+        id={id}
+        value={value}
+        callback={callback}
+        type={showPassword ? "text" : "password"}
+      />
+      <i
+        onClick={() => setShowPassword(!showPassword)}
+        className="absolute bottom-5 right-4 text-black text-lg"
+        aria-label="toggle password visibility"
+        role="button"
+      >
+        {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+      </i>
+    </>
+  );
+};
