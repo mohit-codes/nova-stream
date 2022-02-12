44170b4636250c30bff9a16fd5393693c95132c2
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-12 20:46:52 +0530

    feat: added async function to fetch user data

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,13 +1,36 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/utility";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../contexts/";
-import { useContext } from "react";
+import { AuthContext, UserDataContext } from "../contexts/";
+import { useContext, useState } from "react";
 
-export const useAuth = (setLoading) => {
-  const { user, setUser, setToken } = useContext(AuthContext);
-
+export const useAuth = () => {
+  const { user, setUser, setToken, token } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { userDispatch } = useContext(UserDataContext);
+
+  if (token) {
+    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
+  }
+
+  async function fetchUserData() {
+    setLoading(true);
+    if (user?._id) {
+      const { data } = await axios.get(`${BASE_URL}/user/user-data`);
+      if (data.success) {
+        userDispatch({
+          type: "INITIALIZE_DATA",
+          payload: {
+            playlists: data.playlists,
+            history: data.history,
+            liked: data.liked,
+          },
+        });
+      }
+    }
+    setLoading(false);
+  }
 
   async function loginWithUserCredentials(email, password) {
     setLoading(true);
@@ -18,7 +41,15 @@ export const useAuth = (setLoading) => {
     if (data.success) {
       setUser(data.user);
       setToken(data.token);
-      axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+      userDispatch({
+        type: "INITIALIZE_DATA",
+        payload: {
+          playlists: data.playlists,
+          history: data.history,
+          liked: data.liked,
+        },
+      });
+      axios.defaults.headers.common["authorization"] = `Bearer ${data.token}`;
       localStorage.setItem("user", JSON.stringify(data.user));
       localStorage.setItem("token", JSON.stringify(data.token));
       navigate("/", { replace: true });
@@ -36,7 +67,7 @@ export const useAuth = (setLoading) => {
     if (data.success) {
       setUser(data.user);
       setToken(data.token);
-      axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+      axios.defaults.headers.common["authorization"] = `Bearer ${data.token}`;
       localStorage.setItem("user", JSON.stringify(data.user));
       localStorage.setItem("token", JSON.stringify(data.token));
       navigate("/", { replace: true });
@@ -52,9 +83,11 @@ export const useAuth = (setLoading) => {
   }
 
   const logout = () => {
-    axios.defaults.headers.common["Authorization"] = null;
-    localStorage?.removeItem("user");
-    localStorage?.removeItem("token");
+    setUser(null);
+    setToken(null);
+    axios.defaults.headers.common["authorization"] = null;
+    userDispatch({ type: "ERASE" });
+    localStorage.clear();
     navigate("/", { replace: true });
   };
 
@@ -64,5 +97,7 @@ export const useAuth = (setLoading) => {
     signupWithUserCredentials,
     emailValidate,
     logout,
+    loading,
+    fetchUserData,
   };
 };
