5169d0f5b54362481fcdddb2ce424e92d4d9caab
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-12 20:45:37 +0530

    refactor: added more props to component

--- src/containers/NormalVideoListing.js
+++ src/containers/NormalVideoListing.js
@@ -3,16 +3,22 @@ import { Loading } from "../components/Loading";
 import ScrollContainer from "react-indiana-drag-scroll";
 import { VideoCard } from "../components/VideoCards/VideoCard";
 
-export const NormalVideoListing = ({ title, showTitle = true, videos }) => {
+export const NormalVideoListing = ({
+  title,
+  showTitle = true,
+  videos,
+  dontShowTitle = false,
+}) => {
   const { loading } = useVideo();
 
   return (
     <div className="border-b border-gray-400 py-4">
-      {showTitle ? (
-        <p className="font-semibold text-2xl ml-4">{`From ${videos[0]?.owner}`}</p>
-      ) : (
-        <p className="font-semibold text-2xl ml-4">{`More from ${title}`}</p>
-      )}
+      {!dontShowTitle &&
+        (showTitle ? (
+          <p className="font-semibold text-2xl ml-4">{`From ${videos[0]?.owner}`}</p>
+        ) : (
+          <p className="font-semibold text-2xl ml-4">{`More from ${title}`}</p>
+        ))}
       <ScrollContainer className="flex justify-start items-start">
         {loading ? (
           <Loading />
