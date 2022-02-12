2b17bfd5709a991d14de7b81e8f61c75a5c0dccf
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-12 20:49:33 +0530

    feat: added buttons to video page

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -4,11 +4,16 @@ import { Loading } from "../components/Loading";
 import { useVideo } from "../hooks/useVideo";
 import { VideoSection } from "../containers/VideoSection";
 import { NormalVideoListing } from "../containers/NormalVideoListing";
+import { LikeButton } from "../components/LikeButton";
+import { MdPlaylistAdd } from "react-icons/md";
+import { PlayListModal } from "../components/PlaylistModal/PlayListModal";
 
 export const VideoPage = () => {
   const { id } = useParams();
   const { fetchSingleVideo, loading, videoList } = useVideo();
   const [video, setVideo] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
   const filteredByOrg = videoList.filter((v) => v?.owner === video?.owner);
 
   useEffect(() => {
@@ -27,10 +32,16 @@ export const VideoPage = () => {
       ) : (
         <div>
           <VideoSection video={video} />
-          <div className="flex ">
+          <div className="flex justify-between pr-10">
             <div className="w-1/2">
               <h2 className="m-4 text-xl font-semibold">{video?.title}</h2>
               <p className="m-4">{video?.description}</p>
+            </div>
+            <div className="flex justify-between w-40 items-start">
+              <LikeButton id={video?._id} />
+              <button aria-label="add to playlist" onClick={openModal}>
+                <MdPlaylistAdd className="text-2xl" />
+              </button>
             </div>
           </div>
           <NormalVideoListing
@@ -39,6 +50,9 @@ export const VideoPage = () => {
             showTitle={false}
           />
         </div>
+      )}
+      {showModal && (
+        <PlayListModal setShowModal={setShowModal} id={video?._id} />
       )}
     </div>
   );
